b3034361deeee31ac09a12e07db81fb10702c3f4
qrg <qualle.rhythmus+d@gmail.com>
2021-07-20 04:39:22 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  plugins: [reactRefresh()],
+})
